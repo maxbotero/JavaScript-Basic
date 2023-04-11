63f2408c5f0fc226f0e9e22659ebea1e932d9813
maxbotero <124061834+maxbotero@users.noreply.github.com>
2023-04-11 15:06:54 -0500

    Create lista.js

---
+++ 2-Sintaxis/lista.js
@@ -0,0 +1,8 @@
+const libro = {
+    titulo: "100 años de soledad",
+    autor: "Gabriel Garcia Marquez",
+    fecha: "mayo de 1967",
+    url: "https://www.secst.cl/upfiles/documentos/19072016_1207am_578dc39115fe9.pdf"
+}
+const miLista = ["Max",45,true, new Date("december 31 1978"),libro];
+console.log(miLista);
