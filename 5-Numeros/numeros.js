854b3ea839eae71ae1e3126d32d474c8737b826f
maxbotero <124061834+maxbotero@users.noreply.github.com>
2023-04-12 15:17:13 -0500

    Create numeros.js

---
+++ 5-Numeros/numeros.js
@@ -0,0 +1,9 @@
+let alturaC = 167;
+let alturaM = 1.67;
+let pesoK = 54.5;
+let alturaMRed = Math.ceil(alturaM);
+let pesoKRed = Math.floor(pesoK);
+let maximo = Number.MAX_VALUE == Number.MAX_VALUE + 1;
+console.log(alturaMRed);
+console.log(pesoKRed);
+console.log(maximo);
