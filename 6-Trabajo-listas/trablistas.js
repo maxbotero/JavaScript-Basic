8be18abd3776951b07f947e424fc96c1b5d9e0f4
maxbotero <124061834+maxbotero@users.noreply.github.com>
2023-04-13 16:18:24 -0500

    Update trablistas.js

--- 6-Trabajo-listas/trablistas.js
+++ 6-Trabajo-listas/trablistas.js
@@ -5,7 +5,7 @@ mercado.pop()
 //console.log(mercado)
 let peliculas = [ {titulo: "The Secret Garden", director: "Agnieszka Holland", fecha: new Date("1993-08-13")}, {titulo: "The Lord of the Rings: The Fellowship of the Ring", director: "Peter Jackson", fecha: new Date("2001-12-25")}, {titulo: "The Hunt", director: "Craig Zobel", fecha: new Date("2020-03-11")} ]
 //console.log(peliculas)
-let nuevaLista = peliculas.filter(valor => valor.fecha < new Date("2010-01-01"))
+let nuevaLista = peliculas.filter(valor => valor.fecha > new Date("2010-01-01"))
 //console.log(nuevaLista)
 let directores = peliculas.map(valor => valor.director)
 //console.log(directores)
