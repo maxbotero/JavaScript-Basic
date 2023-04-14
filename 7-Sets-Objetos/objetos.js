22276dc05164a2d89560c2075fb7e2ef66573b00
maxbotero <124061834+maxbotero@users.noreply.github.com>
2023-04-14 13:30:35 -0500

    Update objetos.js

--- 7-Sets-Objetos/objetos.js
+++ 7-Sets-Objetos/objetos.js
@@ -1,7 +1,7 @@
 const obj = {nombre:"Maximiliano", apellido:"Botero", edad:45, altura:1.67, desarrollador:true}
 let edad = obj.edad
-console.log(edad)
-let lista = [obj, {nombre:"Blanca", apellido:"Pizarro", edad:54}, {nombre:"Mario", apellido:"Usma", edad:43}]
-console.log(lista)
+//console.log(edad)
+let lista = [{...obj}, {nombre:"Blanca", apellido:"Pizarro", edad:54, altura:1.63,desarrollador:false}, {nombre:"Mario", apellido:"Usma", edad:43, altura:1.66, desarrollador:false}]
+//console.log(lista)
 let listaOrdenada = lista.sort((a,b) => b.edad-a.edad)
 console.log(listaOrdenada)
