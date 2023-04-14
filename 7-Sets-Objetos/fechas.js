b6d260ed9ddb38a1448ed18451bebc0c36b86016
maxbotero <124061834+maxbotero@users.noreply.github.com>
2023-04-14 13:48:19 -0500

    Create fechas.js

---
+++ 7-Sets-Objetos/fechas.js
@@ -0,0 +1,6 @@
+let hoy = new Date()
+let nac = new Date('December 01, 1978')
+let mastarde = hoy.getTime() > nac.getTime()
+let dia = nac.getDate()
+let mes = nac.getMonth() + 1
+let anyo = nac.getFullYear()
