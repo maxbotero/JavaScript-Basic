c6d91979c6bbad4911bfdc2c632a5603505f091d
maxbotero <124061834+maxbotero@users.noreply.github.com>
2023-04-21 16:27:49 -0500

    Create textos.js

---
+++ 13-Eslint/textos.js
@@ -0,0 +1,18 @@
+let nombre = "Max"
+let apellido = `Botero`
+let estudiante = nombre + ' ' + apellido
+let estudianteMayus = estudiante.toUpperCase()
+console.log(estudianteMayus)
+let estudianteMinus = estudiante.toLowerCase()
+console.log(estudianteMinus)
+let letrasEstudiante = estudiante.length
+console.log(letrasEstudiante)
+let primeraLetra = nombre.charAt(0)
+console.log(primeraLetra)
+let ultimaLetra = apellido.charAt(apellido.length - 1)
+console.log(ultimaLetra)
+let sinEspacios = estudiante.trim()
+console.log(sinEspacios)
+let estaContenido = estudiante.includes(nombre)
+console.log(estaContenido)
+console.log(estaContenido)
